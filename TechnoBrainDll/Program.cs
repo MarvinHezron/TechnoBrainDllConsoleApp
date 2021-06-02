fe9e1005f52b1c0c0d0a0ce2379c10e0c1e2fb29
Marvin Hezron <marvinrozen@gmail.com>
2021-06-02 07:18:18 +0300

    Add project files.

---
+++ TechnoBrainDll/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TechnoBrainDll
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
